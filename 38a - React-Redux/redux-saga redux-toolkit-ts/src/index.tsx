b70f0f06a461f845d6bfc1a9b388d10bac9d8f67
tranledienlam <lam.tranledien@gmail.com>
2024-03-14 19:59:37 +0700

    update 38a redux saga

--- 38a - React-Redux/redux-saga redux-toolkit-ts/src/index.tsx
+++ 38a - React-Redux/redux-saga redux-toolkit-ts/src/index.tsx
@@ -4,7 +4,6 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import store from './redux/store'
-import reportWebVitals from "./reportWebVitals";
 
 
 const root = ReactDOM.createRoot(
@@ -18,7 +17,3 @@ root.render(
     </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
